cf73eebfc0199faac92ea7cf4825756ba6f64b6f
KartikShrivastava <shrivastavakartik19@gmail.com>
2023-01-15 03:12:09 +0530

    Add author model functionality and tests

--- base_app/models.py
+++ base_app/models.py
@@ -2,15 +2,60 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.db import DatabaseError
+from django.core.exceptions import ObjectDoesNotExist
+
+# This class handles all queries related to Author model
+class AuthorManager(models.Manager):
+    def insert_author(self, author):
+        queryset = self.get_queryset()
+        try:
+            return queryset.create(name=author.name, is_popular=author.is_popular)
+        except DatabaseError as e:
+            raise e
+
+    def get_author_count(self):
+        queryset = self.get_queryset()
+        return queryset.count()
+
+    def get_author_with_exact_name(self, name):
+        queryset = self.get_queryset()
+        try:
+            return queryset.filter(name=name).get()
+        except ObjectDoesNotExist as e:
+            raise e
+
+    # this search is not case-sensitive
+    def get_all_authors_with_similar_name(self, name):
+        queryset = self.get_queryset()
+        authors = queryset.filter(name__icontains=name)
+        return authors
+
+    def update_author_name(self, old_author_name, new_author_name):
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(name=old_author_name).update(name=new_author_name)
+        return rows_affected
+
+    def update_author_popularity(self, author_name, is_popular):
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(name=author_name).update(is_popular=is_popular)
+        return rows_affected
+    
+    def delete_author(self, author_name):
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(name=author_name).delete()[0]
+        return rows_affected
 
 class Author(models.Model):
     author_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(unique=True, max_length=200, help_text='Full name of author')
     is_popular = models.BooleanField()
 
+    objects = AuthorManager()
+
     def __str__(self) -> str:
         return f'{self.name}'
-    
+
 class Book(models.Model):
     owl_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
--- base_app/tests/test_models.py
+++ base_app/tests/test_models.py
@@ -2,6 +2,70 @@ from django.test import TestCase
 from django.db import models
 from datetime import datetime, timedelta
 from base_app.models import Author, Book, BookCopy, LibraryUser, BorrowRecord
+from django.db import DatabaseError
+from django.core.exceptions import ObjectDoesNotExist
+
+class AuthorManagerTest(TestCase):
+    @classmethod
+    def setUp(cls):
+        # insert one entry in the database before each test
+        cls.author = Author(name='James Gosling', is_popular=True)
+        Author.objects.insert_author(cls.author)
+
+    def test_insert_author_successful_insertion(self):
+        author = Author(name='Robert C. Martin', is_popular=False)
+        returned_author = Author.objects.insert_author(author)
+        self.assertEqual(returned_author.name, author.name)
+
+    def test_insert_author_raises_exception(self):
+        author1 = Author(name='Robert C. Martin', is_popular=False)
+        Author.objects.insert_author(author1)
+        author2 = Author(name='Robert C. Martin', is_popular=True)
+        self.assertRaises(DatabaseError, Author.objects.insert_author, author2)
+
+    def test_get_author_count(self):
+        self.assertEqual(Author.objects.get_author_count(), 1)
+    
+    def test_get_author_with_exact_name_returns_valid_author(self):
+        returned_author = Author.objects.get_author_with_exact_name(self.author.name)
+        self.assertEqual(returned_author.name, self.author.name)
+        self.assertEqual(returned_author.is_popular, self.author.is_popular)
+
+    def test_get_author_with_exact_name_raises_exception(self):
+        test_name = 'John Doe'
+        self.assertRaises(ObjectDoesNotExist, Author.objects.get_author_with_exact_name, test_name)
+
+    def test_get_all_authors_with_similar_name_returns_valid_authors(self):
+        Author.objects.create(name='William S. Vincent', is_popular=False)
+        Author.objects.create(name='William C. Wake', is_popular=True)
+
+        search_name = 'william'
+        authors = list(Author.objects.get_all_authors_with_similar_name(search_name))
+
+        self.assertEqual(len(authors), 2)
+
+        for author in authors:
+            self.assertTrue(search_name in author.name.lower())
+
+    def test_update_author_name(self):
+        old_name = self.author.name
+        new_name = 'James Arthur Gosling'
+        rows_affected = Author.objects.update_author_name(old_name, new_name)
+        self.assertEqual(rows_affected, 1)
+        self.assertRaises(ObjectDoesNotExist, Author.objects.get_author_with_exact_name, old_name)
+    
+    def test_update_author_popularity(self):
+        old_is_popular = self.author.is_popular
+        rows_affected = Author.objects.update_author_popularity(self.author.name, not old_is_popular)
+        self.assertEqual(rows_affected, 1)
+        updated_author = Author.objects.get_author_with_exact_name(self.author.name)
+        self.assertEqual(updated_author.is_popular, not old_is_popular)
+
+    def test_delete_author_successful_deletion(self):
+        record_count = Author.objects.get_author_count()
+        rows_affected = Author.objects.delete_author(self.author.name)
+        self.assertEqual(rows_affected, 1)
+        self.assertEqual(Author.objects.filter(name=self.author.name).count(), record_count-1)
 
 class AuthorModelTest(TestCase):
     @classmethod
