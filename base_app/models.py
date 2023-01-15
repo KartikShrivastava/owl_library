0eb39bca6da04207e2defdad393396039c319ca2
KartikShrivastava <shrivastavakartik19@gmail.com>
2023-01-15 19:26:20 +0530

    Add book model functionality and tests

--- base_app/models.py
+++ base_app/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import DatabaseError, models
 
 
@@ -19,6 +19,8 @@ class AuthorManager(models.Manager):
         queryset = self.get_queryset()
         return queryset.count()
 
+    # (Recommended) use only if author with given name exist, instead use
+    # get_all_authors_with_similar_name to check author(s) in library with similar name
     def get_author_with_exact_name(self, name):
         queryset = self.get_queryset()
         try:
@@ -59,10 +61,73 @@ class Author(models.Model):
         return f'{self.name}'
 
 
+class BookManager(models.Manager):
+    def insert_book(self, book):
+        if book.title == None or len(book.title) == 0:
+            raise ValidationError('Cannot insert book with an empty title')
+
+        queryset = self.get_queryset()
+        try:
+            return queryset.create(title=book.title, author=book.author)
+        # raise ObjectDoesNotExist when book.author is null
+        except ObjectDoesNotExist as e:
+            raise e
+
+    def get_book_with_owl_id(self, owl_id):
+        queryset = self.get_queryset()
+        try:
+            return queryset.filter(owl_id=owl_id).get()
+        except ObjectDoesNotExist as e:
+            raise e
+
+    # (Recommended) use only if book with given title exist, instead use
+    # get_all_books_with_similar_title to check availability of book(s) with similar title
+    def get_book_with_exact_title(self, book_title):
+        queryset = self.get_queryset()
+        try:
+            return queryset.filter(title=book_title).get()
+        except ObjectDoesNotExist as e:
+            raise e
+
+    # search is not case_sensitive
+    def get_all_books_with_similar_title(self, book_title):
+        queryset = self.get_queryset()
+        books = queryset.filter(title__icontains=book_title)
+        return books
+
+    def get_all_books_with_similar_author_name(self, author_name):
+        queryset = self.get_queryset()
+        books = queryset.filter(author__name__icontains=author_name)
+        return books
+
+    def get_all_books(self):
+        queryset = self.get_queryset()
+        return queryset.all()
+
+    def update_book_title(self, owl_id, new_book_title):
+        if len(new_book_title) == 0:
+            raise ValidationError('Cannot update book title with an empty string')
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(owl_id=owl_id).update(title=new_book_title)
+        return rows_affected
+
+    def update_book_author(self, owl_id, new_book_author):
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(owl_id=owl_id).update(author=new_book_author)
+        return rows_affected
+
+    def delete_book(self, owl_id):
+        queryset = self.get_queryset()
+        rows_affected = queryset.filter(owl_id=owl_id).delete()[0]
+        return rows_affected
+
+
 class Book(models.Model):
     owl_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.PROTECT)
+
+    objects = BookManager()
 
     class Meta:
         unique_together = ('title', 'author')
--- base_app/tests/test_models.py
+++ base_app/tests/test_models.py
@@ -1,6 +1,7 @@
+import uuid
 from datetime import datetime, timedelta
 
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import DatabaseError, models
 from django.test import TestCase
 
@@ -19,11 +20,11 @@ class AuthorManagerTest(TestCase):
         returned_author = Author.objects.insert_author(author)
         self.assertEqual(returned_author.name, author.name)
 
-    def test_insert_author_raises_exception(self):
+    def test_insert_author_raises_exception_for_duplicate_name(self):
         author1 = Author(name='Robert C. Martin', is_popular=False)
         Author.objects.insert_author(author1)
         author2 = Author(name='Robert C. Martin', is_popular=True)
-        self.assertRaises(DatabaseError, Author.objects.insert_author, author2)
+        self.assertRaises(DatabaseError, Author.objects.insert_author, author=author2)
 
     def test_get_author_count(self):
         self.assertEqual(Author.objects.get_author_count(), 1)
@@ -33,7 +34,7 @@ class AuthorManagerTest(TestCase):
         self.assertEqual(returned_author.name, self.author.name)
         self.assertEqual(returned_author.is_popular, self.author.is_popular)
 
-    def test_get_author_with_exact_name_raises_exception(self):
+    def test_get_author_with_exact_name_raises_exception_if_author_doesnt_exist(self):
         test_name = 'John Doe'
         self.assertRaises(ObjectDoesNotExist, Author.objects.get_author_with_exact_name,
                           test_name)
@@ -82,13 +83,140 @@ class AuthorModelTest(TestCase):
         self.assertEqual(str(self.author), f'{self.author.name}')
 
 
+class BookManagerTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = Author.objects.insert_author(author=Author(name='Guido van Rossum',
+                                                  is_popular=False))
+
+    def test_insert_book_successful_insertion(self):
+        title = 'An Introduction to Python'
+        book = Book(title=title, author=self.author)
+        inserted_book = Book.objects.insert_book(book=book)
+        self.assertEqual(inserted_book.title, title)
+
+    def test_insert_book_raises_exception_for_missing_author(self):
+        book = Book(title='An Introduction to Python', author=None)
+        self.assertRaises(ObjectDoesNotExist, Book.objects.insert_book, book=book)
+
+    def test_insert_book_raises_exception_for_missing_title(self):
+        book = Book(title=None, author=self.author)
+        self.assertRaises(ValidationError, Book.objects.insert_book, book=book)
+
+    def test_insert_book_raises_exception_for_empty_title(self):
+        book = Book(title='', author=self.author)
+        self.assertRaises(ValidationError, Book.objects.insert_book, book=book)
+
+    def test_get_book_with_owl_id_returns_valid_book(self):
+        book = Book(title='An Introduction to Python', author=self.author)
+        inserted_book = Book.objects.insert_book(book=book)
+        search_result = Book.objects.get_book_with_owl_id(owl_id=inserted_book.owl_id)
+        self.assertEqual(search_result.owl_id, inserted_book.owl_id)
+
+    def test_get_book_with_owl_id_raises_exception_if_book_not_found(self):
+        random_uuid = uuid.uuid4()
+        self.assertRaises(ObjectDoesNotExist, Book.objects.get_book_with_owl_id, random_uuid)
+
+    def test_get_book_with_exact_title_returns_valid_book(self):
+        title = 'An Introduction to Python'
+        book = Book(title=title, author=self.author)
+        Book.objects.insert_book(book=book)
+        search_result = Book.objects.get_book_with_exact_title(book_title=title)
+        self.assertEqual(search_result.title, title)
+    
+    def test_get_book_with_exact_title_raises_exception_if_book_doesnt_exist(self):
+        title = 'An Introduction to Python'
+        self.assertRaises(ObjectDoesNotExist, Book.objects.get_book_with_exact_title,
+                          book_title=title)
+
+    def test_get_all_books_with_similar_title_returns_valid_books(self):
+        title1 = 'Django for APIs: Build web APIs with Python & Django'
+        author1 = Author.objects.insert_author(author=Author(name='William S. Vincent',
+                                                             is_popular=False))
+        Book.objects.insert_book(book=Book(title=title1, author=author1))
+        title2 = 'An Introduction to Python'
+        author2 = self.author
+        Book.objects.insert_book(book=Book(title=title2, author=author2))
+        search_string = 'python'
+        books = list(Book.objects.get_all_books_with_similar_title(book_title=search_string))
+        self.assertEqual(len(books), 2)
+        for book in books:
+            self.assertTrue(search_string in book.title.lower())
+
+    def test_get_all_books_with_similar_author_name_returns_valid_books(self):
+        title1 = 'Django for APIs: Build web APIs with Python & Django'
+        author1 = Author.objects.insert_author(author=Author(name='William S. Vincent',
+                                                             is_popular=False))
+        Book.objects.insert_book(book=Book(title=title1, author=author1))
+        title2 = 'Refactoring Workbook'
+        author2 = Author.objects.insert_author(author=Author(name='William C. Wake',
+                                                             is_popular=False))
+        Book.objects.insert_book(book=Book(title=title2, author=author2))
+        search_string = 'william'
+        books = list(Book.objects
+                     .get_all_books_with_similar_author_name(author_name=search_string))
+        self.assertEqual(len(books), 2)
+        for book in books:
+            self.assertTrue(search_string in book.author.name.lower())
+
+    def test_get_all_books(self):
+        author1 = self.author
+        book1 = Book(title='An Introduction to Python', author=author1)
+        author2 = Author.objects.insert_author(author=Author(name='William S. Vincent',
+                                                             is_popular=False))
+        book2 = Book(title='Django for APIs: Build web APIs with Python & Django',
+                     author=author2)
+        author3 = Author.objects.insert_author(author=Author(name='James Gosling',
+                                                             is_popular=True))
+        book3 = Book(title='The Java Language Specification', author=author3)
+        Book.objects.insert_book(book1)
+        Book.objects.insert_book(book2)
+        Book.objects.insert_book(book3)
+        books = list(Book.objects.get_all_books())
+        self.assertEqual(len(books), 3)
+    
+    def test_update_book_title_successful_updation(self):
+        old_title = 'Intro to Python'
+        inserted_book = Book.objects.insert_book(book=
+                                                 Book(title=old_title, author=self.author))
+        new_title = 'An Introduction to Python'
+        rows_affected = Book.objects.update_book_title(inserted_book.owl_id, new_title)
+        self.assertEqual(rows_affected, 1)
+    
+    def test_update_book_title_raises_exception_for_empty_title(self):
+        old_title = 'An Introduction to Python'
+        inserted_book = Book.objects.insert_book(book=
+                                                 Book(title=old_title, author=self.author))
+        new_title = ''
+        self.assertRaises(ValidationError,
+                          Book.objects.update_book_title,
+                          inserted_book.owl_id, new_title)
+
+    def test_update_book_author_successful_updation(self):
+        old_author = self.author
+        title = 'A Tour of C++'
+        book = Book(title=title, author=old_author)
+        inserted_book = Book.objects.insert_book(book=book)
+        new_author = Author.objects.insert_author(author=Author(name='Bjarne Stroustrup',
+                                                  is_popular=False))
+        rows_affected = Book.objects.update_book_author(inserted_book.owl_id,
+                                                        new_book_author=new_author)
+        self.assertEqual(rows_affected, 1)
+
+    def test_delete_book_successful_deletion(self):
+        book = Book(title='An Introduction to Python', author=self.author)
+        inserted_book = Book.objects.insert_book(book=book)
+        rows_affected = Book.objects.delete_book(inserted_book.owl_id)
+        self.assertEqual(rows_affected, 1)
+
+
 class BookModelTest(TestCase):
     @classmethod
-    def setUpTestData(cls) -> None:
+    def setUpTestData(cls):
         cls.author = Author.objects.create(name='Napoleon Hill', is_popular=False)
         cls.book = Book.objects.create(title='Think And Grow Rich', author=cls.author)
 
-    def test_object_name(self) -> None:
+    def test_object_name(self):
         self.assertEqual(str(self.book), f'{self.book.title}')
 
 
